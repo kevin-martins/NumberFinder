8cd48ac712ec25b253226f9200384aa459ee1550
unknown <kev.martinsvieira@gmail.com>
2021-04-19 14:22:33 +0200

    Adding style on tab, help function (nbr higher or lower), Reorganising variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+//#region Variables
+
+const valueError = document.getElementById("valueError");
+const checkButton = document.getElementById("btn");
+const countDoc = document.getElementById("count");
+const answerDoc = document.getElementById("answer");
+const scoreDoc = document.getElementById("score");
+const printNbrUsed = document.getElementById("listNumber");
+const helpDoc = document.getElementById("help");
+let tries = 1;
+let score = 0;
+var nbrUsed = [];
+let nbr = 0;
+
+//#endregion
+
+//#region Min/Max value selection
 const min = () => {
     return parseInt(document.getElementById('min').value);
 };
@@ -10,9 +27,6 @@ const error = () => {
     return min() >= max() ? true:false;
 };
 
-const valueError = document.getElementById("valueError");
-const checkButton = document.getElementById("btn");
-
 const invalidNumbers = () => {
     if (!error()) {
         valueError.innerHTML = '';
@@ -23,76 +37,142 @@ const invalidNumbers = () => {
     }
 };
 
+//#endregion
+
+//#region Generation of random number
+
 const random = () => {
     return Math.floor(Math.random() * (max() - min() + 1)) + min();
 };
 
-let nbr = random();
+nbr = random();
+
+//#endregion
+
+//#region Easy, Medium and Harcore Button
 
 const easy = () => {
     document.getElementById('min').value = 1;
     document.getElementById('max').value = 10;
     nbr = random();
-}
+};
 
 const medium = () => {
     document.getElementById('min').value = 1;
     document.getElementById('max').value = 20;
     nbr = random();
-}
+};
 
 const hardcore = () => {
     document.getElementById('min').value = 1;
     document.getElementById('max').value = 50;
     nbr = random();
-}
+};
+
+//#endregion
+
+//#region User input
 
 const userInput = () => {
     return parseInt(document.getElementById('inputNbr').value);
 };
 
-const countDoc = document.getElementById("count");
-const answerDoc = document.getElementById("answer");
+//#endregion
+
+//#region Win phrase (how strong/lucky you are)
 
 const answer = (tries) => {
     return tries >= 15? answerDoc.innerHTML = `You loser ${tries} tries !`:
-     tries > 10? answerDoc.innerHTML = `Well well, ${tries} tries. you can do better !` :
-     tries > 5? answerDoc.innerHTML = `Not so bad with ${tries} tries.` : 
-     answerDoc.innerHTML = `OMG YOU CHEATED !! YOU JUST DID ${tries} TRIES !!!`
-}
+     tries > 10? answerDoc.innerHTML = `Well well, ${tries} tries. you could do better !` :
+     tries > 5? answerDoc.innerHTML = `${tries} tries... not so bad.` : 
+     answerDoc.innerHTML = `OMG YOU CHEATED !! YOU DID ${tries} TRIES !!!`
+};
 
-let tries = 1;
+//#endregion
+
+//#region Print tries did
+
 const count = (tries) => {
     return countDoc.innerText = `You did ${tries} tries`;
 };
 
-let scoreDoc = document.getElementById("score");
-let score = 0;
+//#endregion
+
+//#region Score Management
+
 const playerScore = (tries) => {
-    score += 10 * Math.floor((max() - min() + 1)/tries);
+    let newScore = 10 * Math.floor((max() - min() + 1)/tries);
+    score += newScore;
+    countDoc.innerText = `You won ${newScore} points for a total of ${score} points`;
     return scoreDoc.innerHTML = score;
+};
+
+//#endregion
+
+//#region Print on screen last user input or Remove all inputs
+
+const printUsed = lastNbrAdded => {
+    return printNbrUsed.innerHTML += `${lastNbrAdded} `;
+};
+
+const removeUsed = () => {
+    nbrUsed = [];
+    return printNbrUsed.innerHTML = '';
+};
+
+//#endregion
+
+//#region Help the number to find is higher or lower the user input
+
+const help = usrInput => {
+    return usrInput < nbr ? helpDoc.innerHTML = 'Try a number higher':helpDoc.innerHTML = 'Try a number lower' ;
+};
+
+const removeHelp = () => {
+    return helpDoc.innerHTML = '';
+};
+
+//#endregion
+
+// Action when click on "Ckeck" button
+function enterButton() {
+    const usrInput = userInput();
+    if (checkValidNumber() == 0) {
+        nbrUsed.push(usrInput);
+        printUsed(nbrUsed[nbrUsed.length-1]);
+        // Win condition
+        if (usrInput == nbr) {
+            answer(tries);
+            playerScore(tries);
+            removeUsed();
+            removeHelp();
+            tries = 1;
+            nbr = random();
+        } else {
+            help(usrInput);
+            count(tries++);
+        }
+        console.log(nbr);
+    }
 }
 
-var nbrUsed = [];
-
-function enterButton() {
-    if (userInput()) {
+// Check Validity of the user input number
+function checkValidNumber() {
+    const usrInput = userInput();
+    // User Input empty or not == 0
+    if (!usrInput && usrInput != 0) {
+        answerDoc.innerHTML = 'Error: please fill the empty input.';
+    } else {
         answerDoc.innerHTML = '';
+        // User Input number already used
         if (nbrUsed.includes(userInput())) {
             answerDoc.innerHTML = 'Error: number already used.';
-        } else {
-            nbrUsed.push(userInput());
-            // Win condition
-            if (userInput() == nbr) {
-                count(tries);
-                answer(tries);
-                playerScore(tries);
-                nbrUsed = [];
-            } else {
-                count(tries++);
-            }
         }
-    } else {
-        answerDoc.innerHTML = 'Error: please fill the empty input.';
-    }
+        // User input above max or below min
+        if (usrInput > max() || usrInput < min()) {
+            answerDoc.innerHTML = `Error: number should be between ${min()} and ${max()}`
+        }
+    } 
+
+    return !answerDoc.innerHTML? 0:84;
 }
